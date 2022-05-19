eaa37321069930721df70740164ec6450808b8ec
DAVID <davidkirakosyan504@mail.com>
2022-05-19 13:30:52 +0400

    security

---
+++ src/main/java/com/example/companyemployeespring/security/SecurityService.java
@@ -0,0 +1,27 @@
+package com.example.springdemo.security;
+
+import com.example.springdemo.model.User;
+import com.example.springdemo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class SecurityService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<User> byEmail = userRepository.findByEmail(s);
+        if (byEmail.isEmpty()){
+            throw new UsernameNotFoundException("User with " + s +" username dose net exist");
+        }
+        return new CurrentUser(byEmail.get());
+    }
+}
